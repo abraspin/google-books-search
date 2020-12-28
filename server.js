538b50905465faa7d70ef25efdec794944a8e3e2
abraspin <abrahamspindel@gmail.com>
2020-12-28 16:33:32 -0600

    add mongoose and morgan logger

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+// const routes = require("./routes"); TODO: ADD ROUTES TODO:
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger("dev"));
+// TODO: add compression?
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 
 // Define API routes here
+// app.use(routes); TODO: ADD ROUTES TODO:
+
+// Connect to MongoDB Atlas DB with Mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-search", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
