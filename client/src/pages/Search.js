f351ca05a7e9c8c054546ac62659861ea353cea3
abraspin <abrahamspindel@gmail.com>
2021-01-01 16:44:08 -0600

    add search page header and results container

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,7 +3,26 @@ import NavBar from "../Components/NavBar";
 function Search() {
   return (
     <div>
-      <h1>Search Page</h1>
+      <div className="container border p-2">
+        <form>
+          <div className="form-group">
+            <label>Book Search</label>
+            <input
+              type="string"
+              className="form-control"
+              placeholder="Enter book to search"
+            ></input>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Search <i class="fa fa-search fa-sm"></i>
+          </button>
+        </form>
+      </div>
+      <br></br>
+      {/* TODO: Add a ternary operator for if !results.length "No results to display" or something */}
+      <div className="container border">
+        <h3>Results</h3>
+      </div>
     </div>
   );
 }
