c27f76a727b4baa21d21dd882d2ac69164e6b1c1
abraspin <abrahamspindel@gmail.com>
2021-01-01 14:31:16 -0800

    remove app.js boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,34 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Header from "./Components/Header";
+import { useEffect, useState, Fragment } from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
-            </div>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-        </div>
-    );
+  return (
+    <Fragment>
+      <div className="content">
+        <Router>
+          <NavBar> </NavBar>
+          <Header></Header>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/">
+              <Search />
+            </Route>
+            <Route exact path="/Saved">
+              <Saved />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Fragment>
+  );
 }
-
 
 export default App;
