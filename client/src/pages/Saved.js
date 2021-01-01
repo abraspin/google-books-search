29ea38a8de8d0e61c69e4b8f64db1a570a87a6e2
abraspin <abrahamspindel@gmail.com>
2021-01-01 14:29:31 -0800

    add basic search and saved pages

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,11 @@
+import NavBar from "../Components/NavBar";
+
+function Search() {
+  return (
+    <div>
+      <h1>SAVED Page</h1>
+    </div>
+  );
+}
+
+export default Search;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,11 @@
+import NavBar from "../Components/NavBar";
+
+function Search() {
+  return (
+    <div>
+      <h1>Search Page</h1>
+    </div>
+  );
+}
+
+export default Search;
