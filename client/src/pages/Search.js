9d3e55325aaea9e0532fb8b0bfdb870700cabddb
abraspin <abrahamspindel@gmail.com>
2021-01-01 17:33:40 -0600

    mock up results in search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react";
 import NavBar from "../Components/NavBar";
+import ResultItem from "../Components/ResultItem";
+import API from "../utils/API";
 
 function Search() {
+  // Setting our component's initial state
+  const [books, setBooks] = useState([]);
+  const [formObject, setFormObject] = useState({});
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  // Loads all books and sets them to books
+  function loadBooks() {
+    API.getBooks()
+      .then((res) => setBooks(res.data))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div>
       <div className="container border p-2">
@@ -21,7 +40,11 @@ function Search() {
       <br></br>
       {/* TODO: Add a ternary operator for if !results.length "No results to display" or something */}
       <div className="container border">
-        <h3>Results</h3>
+        <h1 className="p-2 font-weight-bold font-italic">Results</h1>
+        <hr></hr>
+        <ResultItem></ResultItem>
+        <ResultItem></ResultItem>
+        <ResultItem></ResultItem>
       </div>
     </div>
   );
