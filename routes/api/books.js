5a0f38e0f5f26f88a676b5068e21cff969997499
abraspin <abrahamspindel@gmail.com>
2021-01-03 17:51:33 -0600

    add book db routes and controller

---
+++ routes/api/books.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/posts"
+router
+  // root
+  .route("/")
+  // get all saved books
+  .get(booksController.findAll)
+  // create new saved book record
+  .post(booksController.create);
+
+// Matches with "/api/posts/:id"
+router
+  .route("/:id")
+  //   helper for delete?
+  .get(booksController.findById)
+  //   update books- unused?
+  .put(booksController.update)
+  //   remove saved book from DB
+  .delete(booksController.remove);
+
+module.exports = router;
