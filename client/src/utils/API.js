89061aba37ec941e104bc5e7d9c10781d0d2f4ba
abraspin <abrahamspindel@gmail.com>
2021-01-04 12:40:58 -0600

    update client API with query

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,25 +1,12 @@
 import axios from "axios";
 
-// /api/books (get) - Should return all saved books as JSON.
-
-// /api/books (post) - Will be used to save a new book to the database.
-
-// /api/books/:id (delete) - Will be used to delete a book from the database by Mongo _id.
-
-// * (get) - Will load your single HTML page in client/build/index.html. Make sure you have this after all other routes are defined.
+// apiKey = config.BOOKS_API_KEY;
 
 export default {
-  // Gets all books
-  // getBooks: function () {
-  //   return axios.get("/api/books");
-  // },
   //Submit a search term to google books API
   bookSearch: function (query) {
     // TODO: Should this have like a try-catch or a .then/.catch?
-    // return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-    return axios.get(
-      `https://www.googleapis.com/books/v1/volumes?q=${query}`
-    );
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   },
   // Return list of all saved books
   getSavedBooks: function () {
@@ -27,9 +14,10 @@ export default {
   },
   // Saves a book to the database
   saveBook: function (bookData) {
+    console.log("🚀 ~ file: API.js ~ line 30 ~ I'm inside save book");
     return axios.post("/api/books", bookData);
   },
-  // Deletes the book with the given id
+  // used to delete a book from the database by Mongo _id.
   deleteBook: function (id) {
     return axios.delete("/api/books/" + id);
   },
