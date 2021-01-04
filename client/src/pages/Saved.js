1e00882274b9a73c47b380f2ad4cf47a7b3c8b8c
abraspin <abrahamspindel@gmail.com>
2021-01-03 20:29:23 -0600

    move saved books to saved page 2

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,6 +1,25 @@
 import NavBar from "../Components/NavBar";
 
 function Search() {
+  // //TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO:TODO: this should go in saved searches I think
+  // // Load all books and store them with setBooks
+  // useEffect(() => {
+  //   loadBooks();
+  // }, []);
+
+  // // Loads all books and sets them to books
+  // function loadBooks() {
+  //   API.getBooks()
+  //     .then((res) => {
+  //       console.log(
+  //         "🚀 ~ file: Search.js ~ line 21 ~ .then ~ res.data",
+  //         res.data
+  //       );
+  //       setBooks(res.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
+
   return (
     <div>
       <h1>SAVED Page</h1>
